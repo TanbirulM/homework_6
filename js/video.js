a8809022f24396c1a3f2cebba2a489ccc4ca99f7
Tanbirul <tanbirum@umich.edu>
2020-11-23 19:03:31 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -6,9 +6,55 @@ window.addEventListener("load", function() {
 
 document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video");
+	video.play();
 });
 
 document.querySelector("#pause").addEventListener("click", function() {
-	
+	video.pause();
+	console.log("Pause Video");
 });
 
+document.querySelector("#slower").addEventListener("click", function() {
+	video.playbackRate = video.playbackRate * .90;
+	console.log("Speed is " + video.playbackRate);
+});
+
+document.querySelector("#faster").addEventListener("click", function() {
+	video.playbackRate = video.playbackRate * 1.25;
+	console.log("Speed is " + video.playbackRate);
+});
+
+document.querySelector("#skip").addEventListener("click", function() {
+	video.currentTime = video.currentTime + 5;
+	if (video.ended) {
+		video.load();
+		video.play();
+	}
+	console.log("Current location is " + video.currentTime);
+});
+
+document.querySelector("#mute").addEventListener("click", function() {
+	if (video.muted) {
+		video.muted = false;
+		document.querySelector("#mute").innerHTML = "Unmute";
+	}
+	else {
+		video.muted = true;
+		document.querySelector("#mute").innerHTML = "Mute";
+	}
+});
+
+document.querySelector("#volumeSlider").addEventListener("click", function() {
+	var volv = document.querySelector("#volume").innerHTML;
+	volv = volumeSlider.value;
+	video.volume = volv/100;
+	document.querySelector("#volume").innerHTML = volv + '%';
+});
+
+document.querySelector("#old").addEventListener("click", function() {
+	video.classList.add("oldTime");
+});
+
+document.querySelector("#original").addEventListener("click", function() {
+	video.classList.remove("oldTime");
+});
